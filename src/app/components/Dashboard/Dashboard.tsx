798da0706fdc974bef56dd5fc6b2dc9fa5bced47
Marvin Kaiser <marvin.kaiser.93@gmail.com>
2026-05-20 13:27:37 +0200

    fix: compatibility fixes for updated dependencies (TS moduleResolution, react-grid-layout v2, fontawesome v7)

--- src/app/components/Dashboard/Dashboard.tsx
+++ src/app/components/Dashboard/Dashboard.tsx
@@ -48,7 +48,7 @@ const Dashboard: React.FC<PropTypes> = props => {
         rowHeight={20}
         draggableHandle=".visContainerDraggableHandle"
         onLayoutChange={layout =>
-          dispatch(globalActions.updateDashboardLayout({ dashboardId, layout }))
+          dispatch(globalActions.updateDashboardLayout({ dashboardId, layout: [...layout] }))
         }
       >
         {props.visualisations.map(visualisation => (
